f4a94b564c858419b97028c662c748449b809b7a
Dean Papastrat <dean.g.papastrat@gmail.com>
2016-06-10 15:31:21 -0400

    Add Users Page and Profile Editing

--- app/lib/GBController.java
+++ app/lib/GBController.java
@@ -17,9 +17,13 @@ public abstract class GBController extends play.mvc.Controller {
     protected FormFactory formFactory;
     private DynamicForm formParams;
     private boolean formParamsLoaded = false;
+    private User currentUser;
 
     public User currentUser() {
-        return Secured.getUser(ctx());
+        if (currentUser == null || !Secured.getCurrentUsername(ctx()).equals(currentUser.email)) {
+            currentUser = Secured.getUser(ctx());
+        }
+        return currentUser;
     }
 
     /**
@@ -39,8 +43,19 @@ public abstract class GBController extends play.mvc.Controller {
      * @param <T> A model object type
      * @return a play form to use in requests
      */
-    public <T> play.data.Form<T> modelForm(Class klass) {
+    public <T> play.data.Form<T> modelForm(Class<T> klass) {
         return (play.data.Form<T>) formFactory.form(klass).bindFromRequest();
+    }
+
+    /**
+     * Creates a model form via the formFactory and fills it with an object
+     * @param object An object to fill the form with
+     * @param <T> A model object type
+     * @return a play form to use in requests
+     */
+    @SuppressWarnings("unchecked")
+    public <T> play.data.Form<T> modelForm(T object) {
+        return ((play.data.Form<T>) formFactory.form(object.getClass())).fill(object);
     }
 
     /**
@@ -49,7 +64,7 @@ public abstract class GBController extends play.mvc.Controller {
      * @param <T> A model object type
      * @return a play form to use in requests
      */
-    public <T> play.data.Form<T> emptyModelForm(Class klass) {
+    public <T> play.data.Form<T> emptyModelForm(Class<T> klass) {
         return (play.data.Form<T>) formFactory.form(klass);
     }
 }
